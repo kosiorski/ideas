f2e9355dd98ed0b1d2c2b28feae70eac1e2dd6e6
k0sa <kosiorski.tomasz@gmail.com>
2019-01-10 22:52:16 +0100

    Refactored.

--- src/main/java/pl/kosiorski/config/SecurityConfiguration.java
+++ src/main/java/pl/kosiorski/config/SecurityConfiguration.java
@@ -53,12 +53,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
+
                 .defaultSuccessUrl("/homepage")
                 .usernameParameter("login")
                 .passwordParameter("password")
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/").and().exceptionHandling()
+                .logoutSuccessUrl("/homepage").and().exceptionHandling()
                 .accessDeniedPage("/access-denied");
     }
 
--- src/main/java/pl/kosiorski/controller/HomepageController.java
+++ src/main/java/pl/kosiorski/controller/HomepageController.java
@@ -1,14 +1,36 @@
 package pl.kosiorski.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kosiorski.model.User;
+import pl.kosiorski.service.UserService;
 
 @Controller
+@RequestMapping(value = {"", "/homepage"})
 public class HomepageController {
+
+  private final UserService userService;
+
+  public HomepageController(UserService userService) {
+    this.userService = userService;
+  }
 
   @GetMapping
   public String userHome(Model model) {
+    try {
+      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+      User user = userService.findUserByLogin(auth.getName());
+      model.addAttribute(
+          "userName",
+          "Welcome " + user.getLogin() + " (" + user.getEmail() + ") Id:" + user.getId());
+    } catch (Exception e) {
+
+    }
+
     return "/homepage";
   }
 }
