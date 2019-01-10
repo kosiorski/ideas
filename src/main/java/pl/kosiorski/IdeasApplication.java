6327537006cfdabe2ccf507a229dac5f85cfaf5f
k0sa <kosiorski.tomasz@gmail.com>
2019-01-10 18:38:04 +0100

    first commit.

---
+++ src/main/java/pl/kosiorski/IdeasApplication.java
@@ -0,0 +1,14 @@
+package pl.kosiorski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeasApplication.class, args);
+    }
+
+}
+
